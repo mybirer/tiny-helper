5c6c87d8c9846e72cae30c4193d94b834db487d3
M. Yasin Birer <mybirer@gmail.com>
2021-04-24 16:54:19 +0300

    version updated. published on atms

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mybirer:tiny-helper',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Tinymce 5 Module for Meteor.js',
   // URL to the Git repository containing the source code for this package.
